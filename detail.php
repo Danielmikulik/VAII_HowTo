6e06e87a2db86d89d1e9f55443b3f590a8973e0b
Daniel <danecko24@gmail.com>
2020-11-17 19:45:51 +0100

    delete from DB

--- detail.php
+++ detail.php
@@ -30,7 +30,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="edit.php">Edit<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="edit.php?id=<?php echo $_GET['id']?>">Edit<span class="sr-only">(current)</span></a>
                 </li>
 
             </ul>
